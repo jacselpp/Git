29c2803fc232ef97be3791fe413f0ef83bd8330a
Miguel Ricardo Verdecia Velázquez <kyljuru@gmail.com>
2021-11-06 13:10:17 -0400

    modified history view ui

--- lib/ui/views/home/history_view.dart
+++ lib/ui/views/home/history_view.dart
@@ -1,9 +1,10 @@
+import 'package:detooo_recargas/app/app_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:detooo_recargas/models/recargas/orders_model.dart';
 import 'package:detooo_recargas/services/providers/history_provider.dart';
 import 'package:detooo_recargas/ui/app_ui.dart';
-import 'package:floor/floor.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class HistoryView extends StatefulWidget {
   const HistoryView({Key? key}) : super(key: key);
@@ -33,31 +34,27 @@ class _HistoryViewState extends State<HistoryView> {
     _context ??= context;
     return HomeLayout(
       scrollController: _scrollController,
-      child: Container(
-        color: Theme.of(context).cardColor,
-        child: FutureBuilder(
-          future: context.watch<HistoryProvider>().fetchOrdersPage(),
-          initialData: const <Orders>[],
-          builder:
-              (BuildContext context, AsyncSnapshot<List<Orders>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done &&
-                snapshot.hasData) {
-              return _buildItems(snapshot.data);
-            }
+      child: FutureBuilder(
+        future: context.watch<HistoryProvider>().fetchOrdersPage(),
+        initialData: const <Orders>[],
+        builder: (BuildContext context, AsyncSnapshot<List<Orders>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData) {
+            return _buildItems(snapshot.data);
+          }
 
-            if (snapshot.connectionState == ConnectionState.waiting &&
-                context.read<HistoryProvider>().orders.isNotEmpty) {
-              return Column(
-                children: [
-                  _buildItems(snapshot.data),
-                  _buildLoading(),
-                ],
-              );
-            } else {
-              return _buildLoading();
-            }
-          },
-        ),
+          if (snapshot.connectionState == ConnectionState.waiting &&
+              context.read<HistoryProvider>().orders.isNotEmpty) {
+            return Column(
+              children: [
+                _buildItems(snapshot.data),
+                _buildLoading(),
+              ],
+            );
+          } else {
+            return _buildLoading();
+          }
+        },
       ),
     );
   }
@@ -75,13 +72,63 @@ class _HistoryViewState extends State<HistoryView> {
   }
 
   Widget _buildItem(List<Orders?> list, int index) {
+    final locale = AppLocalizations.of(context)!;
+    Orders item = list[index]!;
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
+      child: SizedBox(
         width: ScreenHelper.screenWidth(context),
-        height: 100,
-        color: Colors.lime,
-        child: Text('$index'),
+        // color: Colors.lime,
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            item.items![0].item!.description!.title!,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          Text(
+                            item.items![0].item!.description!.subtitle!,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: Theme.of(context).textTheme.subtitle1,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        _handleIcon(item.status),
+                        Text('${item.amount} ${item.currency}'),
+                      ],
+                    ),
+                  ],
+                ),
+                const Divider(),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                        '${locale.read('date')}: ${item.createdAt!.split('T')[0]}'),
+                    Text(
+                        '${locale.read('time')}: ${item.createdAt!.split('T')[1].split('Z')[0]}'),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -100,5 +147,23 @@ class _HistoryViewState extends State<HistoryView> {
         child: CircularProgressIndicator(),
       ),
     );
+  }
+
+  Widget _handleIcon(String? status) {
+    late Icon icon;
+    switch (status) {
+      case 'success':
+        icon = const Icon(
+          Icons.check_circle,
+          color: Colors.green,
+        );
+        break;
+      default:
+        icon = const Icon(
+          Icons.refresh,
+          color: Colors.yellow,
+        );
+    }
+    return icon;
   }
 }
